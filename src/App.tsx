9671359930147f5cec3c19313ffeb3e6800d7098
GLiklikadze <102631687+GLiklikadze@users.noreply.github.com>
2024-11-30 19:33:07 +0400

    add question page jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { Routes,Route} from 'react-router-dom';
-import DefaultLayout from './layouts/default/layoutdefault';
-import Home from './pages/home/home';
-import { SignUp } from './pages/signUp';
-import {  SignIn } from './pages/logIn';
-import Createquestion from './pages/createquestion/createquestion';
+import { Routes, Route } from "react-router-dom";
+import DefaultLayout from "./layouts/default/layoutdefault";
+import Home from "./pages/home/home";
+import { SignUp } from "./pages/signUp";
+import { SignIn } from "./pages/logIn";
+import Createquestion from "./pages/createquestion/createquestion";
+import QuestionPage from "./pages/questionPage";
 
 function App() {
   return (
-   <Routes>
-      <Route element={<DefaultLayout/>}>
-          <Route
-            path='/'
-            element = {
-             <SignUp/>
-            }
-          />
-          <Route path="/signin" element={ <SignIn/>} />
-          <Route path="/home" element={ <Home/>} />
-          <Route path='/createquestion' element={<Createquestion/>}/>
+    <Routes>
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/createquestion" element={<Createquestion />} />
+        <Route path="/question-page" element={<QuestionPage />} />
       </Route>
-      
-   </Routes>
+    </Routes>
   );
 }
 
